e4865c9e4c99dd08399a6d90fa7e4915383de4c8
Wilde <gutopicciani@yahoo.com.br>
2015-06-17 00:54:39 -0300

    Ajustes no front

--- public/index/indexController.js
+++ public/index/indexController.js
@@ -1,13 +1,13 @@
 angular.module('contatos', []);
 angular.module('contatos').controller('indexController', function($scope, $http){
 	
-	var getContatos = function(){
+	var _getContatos = function(){
 		$http.get("http://localhost:3000/contatos").success(function(data, status){
 			$scope.contatos = data;
 		});
 	};
 
-	getContatos();
+	_getContatos();
 
 	$scope.masterClick = function(state){
 		$scope.contatos.forEach(function(contato){
@@ -20,13 +20,31 @@ angular.module('contatos').controller('indexController', function($scope, $http)
 		$scope.errorList.pop();
 	};
 
-	$scope.addContato = function(contato){
-		$http.post("http://localhost:3000/contatos", contato).success(function(data){
-			delete $scope.contato;
-		});
-	};
+	$scope.errorList = [];
 
-	$scope.errorList = ['Teste mensagem de erro criada por diretiva 1', 'Teste 2 mensagem de erro criada por diretiva'];
+	$scope.addContato = function(contato){		//SIM, ESSE CÓDIGO ESTÁ FEIO, MAS ESTÁ FUNCIONAL, DEPOIS ARRUMO.
+		$scope.errorList = [];
+		if (contato){
+			if (contato.nome == undefined || contato.nome == ''){
+				$scope.errorList.push('Nome é obrigatório.');
+			}
+			if (contato.telefone === undefined || contato.telefone === ''){
+				$scope.errorList.push('Telefone é obrigatório.');
+			}
+			if ($scope.errorList.length > 0){
+				return;
+			}
+			$scope.contatos.push(angular.copy(contato)); // Provisório, até que o back-end esteja gerando os ID's
+			delete $scope.ct;
+		} else {
+			$scope.errorList.push('Nome é obrigatório.');
+			$scope.errorList.push('Telefone é obrigatório.');
+		}
+		
+		// $http.post("http://localhost:3000/contatos", contato).success(function(data){
+		// 	delete contato;
+		// });
+	};
 });
 
 
